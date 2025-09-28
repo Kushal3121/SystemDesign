04587ed6dbe8ca9ea58903ac7540ee7a52cd2a54
Kushals-Air <kushalchoudhary3121@gmail.com>
2025-09-28 15:44:05 -0500

    abstract factory pattern

--- 2. Design Patterns/2.1 Creational Patterns/2.1.5 AbstractFactoryPattern.js
+++ 2. Design Patterns/2.1 Creational Patterns/2.1.5 AbstractFactoryPattern.js
@@ -8,3 +8,43 @@
 //     - Used when you need to create groups of related objects that must work together.
 //     - Good for cross-platform apps:
 //         - Example: MacOS UI vs Windows UI → both have Button, Checkbox, Menu, but look different.
+
+// Problem: Client code has OS-specific logic (if windows else if mac)
+class WinButton {
+  render() {
+    console.log('Windows Button');
+  }
+}
+class MacButton {
+  render() {
+    console.log('Mac Button');
+  }
+}
+
+class WinCheckbox {
+  render() {
+    console.log('Windows Checkbox');
+  }
+}
+class MacCheckbox {
+  render() {
+    console.log('Mac Checkbox');
+  }
+}
+
+// Client code
+function renderUI(osType) {
+  let button, checkbox;
+  if (osType === 'windows') {
+    button = new WinButton();
+    checkbox = new WinCheckbox();
+  } else if (osType === 'mac') {
+    button = new MacButton();
+    checkbox = new MacCheckbox();
+  }
+  button.render();
+  checkbox.render();
+}
+
+renderUI('windows');
+renderUI('mac');
