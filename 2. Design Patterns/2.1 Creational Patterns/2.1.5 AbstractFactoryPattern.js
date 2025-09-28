94e98133cf89ca3e4af5346b0629170be32b54a9
Kushals-Air <kushalchoudhary3121@gmail.com>
2025-09-28 15:41:39 -0500

    abstract factory pattern

---
+++ 2. Design Patterns/2.1 Creational Patterns/2.1.5 AbstractFactoryPattern.js
@@ -0,0 +1,5 @@
+// Imagine you go to an IKEA store:
+//     - If you want to furnish your living room, you buy a set → sofa + table + lamp.
+//     - If you want to furnish your bedroom, you buy a set → bed + wardrobe + nightstand.
+//     - You don’t mix them up — each factory produces a family of related products.
+// Abstract Factory provides an interface to create families of related objects without specifying their concrete classes.
