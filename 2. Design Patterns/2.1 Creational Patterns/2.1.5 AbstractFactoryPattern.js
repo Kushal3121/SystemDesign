d54fc8dda61aab29d6fbc9ae92236f37093fa2aa
Kushals-Air <kushalchoudhary3121@gmail.com>
2025-09-28 15:41:48 -0500

    abstract factory pattern

--- 2. Design Patterns/2.1 Creational Patterns/2.1.5 AbstractFactoryPattern.js
+++ 2. Design Patterns/2.1 Creational Patterns/2.1.5 AbstractFactoryPattern.js
@@ -3,3 +3,8 @@
 //     - If you want to furnish your bedroom, you buy a set → bed + wardrobe + nightstand.
 //     - You don’t mix them up — each factory produces a family of related products.
 // Abstract Factory provides an interface to create families of related objects without specifying their concrete classes.
+
+// Technical Importance:
+//     - Used when you need to create groups of related objects that must work together.
+//     - Good for cross-platform apps:
+//         - Example: MacOS UI vs Windows UI → both have Button, Checkbox, Menu, but look different.
