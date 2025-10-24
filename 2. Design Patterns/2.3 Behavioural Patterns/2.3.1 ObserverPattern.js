16cd547bc4def45c180e71675ec6674318b17b91
Kushals-Air <kushalchoudhary3121@gmail.com>
2025-10-23 21:17:39 -0500

    observer

---
+++ 2. Design Patterns/2.3 Behavioural Patterns/2.3.1 ObserverPattern.js
@@ -0,0 +1,9 @@
+// Imagine you subscribe to a YouTube channel.
+//     - Whenever the channel uploads a new video, you get a notification.
+//     - You don’t need to keep checking manually — the channel *notifies you automatically*.
+//     - That’s the Observer Pattern!
+
+// Technical Importance:
+//     - Defines a one-to-many relationship between objects.
+//     - When one object changes state (Subject), all its dependents (Observers) are notified.
+//     - Used heavily in event-driven systems (GUIs, stock tickers, chat apps, etc.)
