9f683feda33ecf250697bb443bf579b318b25542
Kushals-Air <kushalchoudhary3121@gmail.com>
2026-08-09 18:44:42 -0500

    fix type issues

--- 3. Examples/1. Parking Lot/parkinglot/model/entities/Ticket.java
+++ 3. Examples/1. Parking Lot/parkinglot/model/entities/Ticket.java
@@ -1,36 +1,43 @@
 package parkinglot.model.entities;
 
+import java.time.Instant;
+
 import parkinglot.model.enums.SpotType;
+import parkinglot.model.enums.TicketStatus;
 import parkinglot.model.enums.VehicleType;
 
 public class Ticket {
     private final String ticketId;
-    private final long entryTime;
-    private final String status;
+    private final Instant entryTime;
+    private TicketStatus status;
+
+    // Spot snapshot
     private final String spotId;
-    private final String vehicleId;
     private final SpotType spotType;
+
+    // Vehicle snapshot
+    private final String vehicleId;
     private final VehicleType vehicleType;
 
-    public Ticket(String ticketId, String spotId, String vehicleId, long entryTime, String status, SpotType spotType, VehicleType vehicleType) {
+    public Ticket(String ticketId, Vehicle vehicle, Spot spot) {
         this.ticketId = ticketId;
-        this.spotId = spotId;
-        this.vehicleId = vehicleId;
-        this.entryTime = entryTime;
-        this.status = status;
-        this.spotType = spotType;
-        this.vehicleType = vehicleType;
+        this.spotId = spot.getId();
+        this.vehicleId = vehicle.getPlate();
+        this.entryTime = Instant.now();
+        this.status = TicketStatus.ACTIVE;
+        this.spotType = spot.getType();
+        this.vehicleType = vehicle.getType();
     }
 
     public String getTicketId() {
         return ticketId;
     }
 
-    public long getEntryTime() {
+    public Instant getEntryTime() {
         return entryTime;
     }
 
-    public String getStatus() {
+    public TicketStatus getStatus() {
         return status;
     }
 
@@ -48,5 +55,10 @@ public class Ticket {
 
     public VehicleType getVehicleType() {
         return vehicleType;
+    }
+
+    public void setStatus(TicketStatus status) {
+        // This method can be used to update the ticket status (e.g., from ACTIVE to PAID or LOST)
+        this.status = status;
     }
 }
