81de1836512e5b267ff307520393c3474b8a10dc
Kushals-Air <kushalchoudhary3121@gmail.com>
2026-08-09 18:09:26 -0500

    add vehicle class

---
+++ 3. Examples/1. Parking Lot/Vehicle.java
@@ -0,0 +1,17 @@
+public class Vehicle {
+    private final String plate;
+    private final VehicleType type;
+
+    public Vehicle(String plate, VehicleType type) {
+        this.plate = plate;
+        this.type = type;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+}
